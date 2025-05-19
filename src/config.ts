681527f138fda22e5dc2346245eb67f98984a5a6
a <a>
2025-05-19 21:25:16 +0200

    Update: implemented main gaming logic

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,26 @@
-import 'phaser'
+import { WEBGL, Scale } from 'phaser'
 
 import MainScene from './scripts/scenes/mainScene'
 
-const DEFAULT_WIDTH = 1280
+const DEFAULT_WIDTH = 1080
 const DEFAULT_HEIGHT = 720
 
 export default {
-  type: Phaser.WEBGL,
+  type: WEBGL,
   backgroundColor: '#ffffff',
   scale: {
     parent: 'phaser-game',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
     width: DEFAULT_WIDTH,
     height: DEFAULT_HEIGHT,
-    backgroundColor: '#2d2d6d',
+    backgroundColor: '#2d2d6d'
   },
   scene: [MainScene],
   physics: {
     default: 'arcade',
     arcade: {
-      debug: false,
+      debug: false
     }
   }
 }
