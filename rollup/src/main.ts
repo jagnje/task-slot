d5cf6d6f0c67c8ae1385c97788b5d37f25f154e7
Andrej Ivanovic <andrej.ivanovic@gmail.com>
2025-12-15 12:40:07 +0100

    .

--- rollup/src/main.ts
+++ rollup/src/main.ts
@@ -1,24 +1,24 @@
-import { Game, AUTO, Scene } from "phaser";
+import { Game, AUTO, Scene } from 'phaser'
 
 class MainScene extends Scene {
   constructor() {
-    super({ key: "MainScene" });
+    super({ key: 'MainScene' })
   }
 
   preload(): void {
-    this.load.image("logo", "https://labs.phaser.io/assets/sprites/phaser3-logo.png");
+    this.load.image('logo', 'https://labs.phaser.io/assets/sprites/phaser3-logo.png')
   }
 
   create(): void {
-    const logo = this.add.image(400, 300, "logo");
+    const logo = this.add.image(400, 300, 'logo')
     this.tweens.add({
       targets: logo,
       y: 500,
       duration: 1500,
-      ease: "Bounce.easeOut",
+      ease: 'Bounce.easeOut',
       yoyo: true,
       repeat: -1
-    });
+    })
   }
 }
 
@@ -26,8 +26,8 @@ const config = {
   type: AUTO,
   width: 800,
   height: 600,
-  backgroundColor: "#1d1d1d",
+  backgroundColor: '#1d1d1d',
   scene: [MainScene]
-};
+}
 
-new Game(config);
+new Game(config)
